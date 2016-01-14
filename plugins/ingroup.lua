e2f0e54a77087c6d6dfeee3a15f5de680243b82f
amirhmz <amdark77@gmail.com>
2016-01-14 15:13:49 +0330

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -111,7 +111,7 @@ local function show_group_settingsmod(msg, data, target)
     	bots_protection = data[tostring(msg.to.id)]['settings']['lock_bots']
    	end
   local settings = data[tostring(target)]['settings']
-  local text = "Group settings:\nLock group name : "..settings.lock_name.."\nLock group photo : "..settings.lock_photo.."\nLock group member : "..settings.lock_member.."\nflood sensitivity : "..NUM_MSG_MAX.."\nBot protection : "..bots_protection
+   local text = "📊 Group settings :\n🔖 Lock group name : "..settings.lock_name.."\n🌄 Lock group photo : "..settings.lock_photo.."\n👥 Lock group member : "..settings.lock_member.."\n🔢 Flood sensitivity : "..NUM_MSG_MAX.."\n🤖 Bot protection : "..bots_protection
   return text
 end
 
